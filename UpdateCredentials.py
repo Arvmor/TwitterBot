0c5c677340945da7338ccd24478bc92e6e309a11
Arvmor <Arvin.morawej@gmail.com>
2020-07-22 13:13:24 +0430

    Add UpdateCredentials

---
+++ UpdateCredentials.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import credentials
+from sys import argv
+
+
+def writeFile(variableName):
+    with open("./credentials.py", "+w") as fileHandle:
+        for d in variableName:
+            fileHandle.write("%s" % d)
+
+
+# Variables
+newAccounts = []
+
+# Append to Variables
+for i in range(1, len(argv)):
+    if argv[i] != 'EndAccounts':
+        newAccounts.append(argv[i])
+    else:
+        break
+credentials.account.append(newAccounts)
+i += 1
+
+for i in range(i, len(argv)):
+    if argv[i] != 'EndRetweet':
+        credentials.retweetSource.append(argv[i])
+    else:
+        break
+i += 1
+
+for i in range(i, len(argv)):
+    if argv[i] != 'EndFollow':
+        credentials.followIdList.append(argv[i])
+    else:
+        break
+i += 1
+
+for i in range(i, len(argv)):
+    if argv[i] != 'EndError':
+        credentials.errorText.append(argv[i])
+    else:
+        break
+i += 1
+
+# Write to file
+newText = f"""account = {credentials.account}
+retweetSource = {credentials.retweetSource}
+followIdList = {credentials.followIdList}
+errorText = {credentials.errorText}"""
+writeFile(newText)
